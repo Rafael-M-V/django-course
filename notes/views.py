aa9681b8a0e2808ed044ddb836f81bd66fb67617
rmv <raf42@protonmail.com>
2023-11-29 13:13:40 -0300

    Adding code

---
+++ notes/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+
+from .models import Notes
+from .forms import NotesForm
+from django.http import Http404
+from django.http.response import HttpResponseRedirect
+from django.views.generic import CreateView, UpdateView, ListView, DetailView
+from django.views.generic.edit import DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+class NotesDeleteView(DeleteView):
+	model = Notes
+	success_url = '/smart/notes'
+	template_name = 'notes/notes_delete.html'
+
+class NotesUpdateView(UpdateView):
+	model = Notes
+	success_url = '/smart/notes'
+	form_class = NotesForm
+
+class NotesCreateView(CreateView):
+	model = Notes
+	success_url = '/smart/notes'
+	form_class = NotesForm
+
+	def form_valid(self, form):
+		self.object = form.save(commit=False)
+		self.object.user = self.request.user
+		self.object.save()
+		return HttpResponseRedirect(self.get_success_url())
+
+class NotesListView(LoginRequiredMixin, ListView):
+	model = Notes
+	context_object_name = 'notes'
+	login_url = 'login'
+
+	def get_queryset(self):
+		return self.request.user.notes.all()
+
+class NotesDetailView(DetailView):
+	model = Notes
+	context_object_name = 'note'
